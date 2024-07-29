ef4cea45a6eff91058edc5f61e9eaa97c75681bd
AniketDarade07 <113009963+AniketDarade07@users.noreply.github.com>
2024-07-29 17:23:58 +0530

    Updated App.jsx

--- noteapp/src/App.jsx
+++ noteapp/src/App.jsx
@@ -3,15 +3,13 @@ import Header from './components/Header'
 import Notes from './components/Notes'
 
 function App() {
-  
   return (
     <div className='container' >
     <div className='main' >
       <Header/>
       <Notes/>
     </div>
-    </div>
-   
+    </div>  
   )
 }
 
